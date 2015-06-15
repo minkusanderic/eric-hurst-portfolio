65d8c9389a050ff3c62ceaec1248d2a5d8e222c7
Eric Hurst <minkusanderic@gmail.com>
2015-06-15 22:07:40 +0000

    Added class models

--- resume/admin.py
+++ resume/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from resume.models import Education
+from resume.models import Class
 
 admin.site.register(Education)
+admin.site.register(Class)
 # Register your models here.
--- resume/models.py
+++ resume/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
 class Education(models.Model):
-    grad_date = models.DateField("Graduation Date")
     school = models.CharField(max_length=200)
     gpa = models.CharField(max_length=4)
+    grad_date = models.DateField("Graduation Date")
+
+    def __unicode__(self):
+        return self.school
+
+class Class(models.Model):
+    school = models.ForeignKey(Education)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
     
+    def __unicode__(self):
+        return self.name
 # Create your models here.
