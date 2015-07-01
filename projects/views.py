c13cad385c0fcab3bd5141a2ef89785b9a2f47a9
Eric Hurst <minkusanderic@gmail.com>
2015-07-01 20:37:11 +0000

    added project support

--- projects/views.py
+++ projects/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 from projects.models import Project 
 
 def index(request):
-    return render(request, 'projects/list.html')
+    all_projects = Project.objects.all() 
+    context = { 'projects' : all_projects } 
+    return render(request, 'projects/list.html', context)
 
 # Create your views here.
