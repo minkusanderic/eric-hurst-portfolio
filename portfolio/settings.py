20341e0a0a3748c999ed97ac238ab85b7064f7c8
Eric Hurst <minkusanderic@gmail.com>
2015-07-13 19:09:52 +0000

    fix bug with settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -86,7 +86,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
